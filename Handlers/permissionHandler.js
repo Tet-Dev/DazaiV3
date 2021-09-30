df8d54f4334cc385a931051c24d8d156eed6a2b4
John Li (Tet) <tet@tet.moe>
2021-09-29 21:23:13 -0700

    Hmmm Seems to work?

--- Handlers/permissionHandler.js
+++ Handlers/permissionHandler.js
@@ -1,34 +1,33 @@
 let base = {
-	dj: ["forceSkip", "forceDC","shuffle","pauseMusic","resumeMusic"],
-	mod: ["purgeMessages", "mute", "nickLock", "nickLockClear", "unmute", "uwuspeak", "warnUser", "removeWarn","export","viewWarns"],
+	dj: ["forceSkip", "forceDC", "shuffle", "pauseMusic", "resumeMusic"],
+	mod: ["purgeMessages", "mute", "nickLock", "nickLockClear", "unmute", "uwuspeak", "warnUser", "removeWarn", "export", "viewWarns"],
 	extraRole: [],
 	extraRole2: [],
-	everyone: ["playSong", "viewSelfWarns","pvc","nhentai","urban","lewd","notlewd","loop","viewCurve","snipe","flushify","removeFromQueue","nowplaying"]
+	everyone: ["playSong", "viewSelfWarns", "pvc", "nhentai", "urban", "lewd", "notlewd", "loop", "viewCurve", "snipe", "flushify", "removeFromQueue", "nowplaying"]
 };
-let allPerms = ["nowplaying","voteSkip","purgeMessages","mute","uwuspeak","disablecmd","nickLock","nickLockClear","reactionRoleCreate","reactionRoleRemove","warnUser","removeWarn","createRedditSub","clearRedditSub","unmute","viewSelfWarns","viewWarns","forceDC","playSong","pvc","nhentai","urban","lewd","notlewd","loop","viewCurve","snipe","ripEmote","flushify","export","pauseMusic","resumeMusic","shuffle","removeFromQueue"];
+let allPerms = ["nowplaying", "voteSkip", "purgeMessages", "mute", "uwuspeak", "disablecmd", "nickLock", "nickLockClear", "reactionRoleCreate", "reactionRoleRemove", "warnUser", "removeWarn", "createRedditSub", "clearRedditSub", "unmute", "viewSelfWarns", "viewWarns", "forceDC", "playSong", "pvc", "nhentai", "urban", "lewd", "notlewd", "loop", "viewCurve", "snipe", "ripEmote", "flushify", "export", "pauseMusic", "resumeMusic", "shuffle", "removeFromQueue"];
 let bot;
 class PermissionsHandler {
 
-	constructor(sqlHandler,b) {
+	constructor(sqlHandler, b) {
 		bot = b;
 		this.sqlHandler = sqlHandler;
 		this.allPerms = allPerms;
 	}
 	async checkForPerm(mem, permName) {
 		if (!mem.guild.name) mem.guild = await bot.getRESTGuild(mem.guild.id);
-		mem = await bot.getRESTGuildMember(mem.guild.id,mem.id);
+		mem = await bot.getRESTGuildMember(mem.guild.id, mem.id);
 		if ((mem.permissions || mem.permission).has("administrator")) return true;
 
-		let data =await this.sqlHandler.getGuild(mem.guild.id);
+		let data = await this.sqlHandler.getGuild(mem.guild.id);
 		if (!data) return false;
 		if (data.adminRole && mem.roles.filter(x => x === data.adminRole).length == 1) return true;
 		let hasRole = "everyone";
-		// 
 		if (data.djRole && mem.roles.includes(data.djRole)) hasRole = "dj";
 		if (data.extraRole && mem.roles.includes(data.extraRole)) hasRole = "extraRole2";
 		if (data.modRole && mem.roles.includes(data.modRole)) hasRole = "modRole";
 		if (data.extraRole2 && mem.roles.includes(data.extraRole2)) hasRole = "extraRole2";
-		
+
 		let inherits = {
 			everyone: (data.everyonePerms ? data.everyonePerms.split(",") : base.everyone).includes(permName),
 			dj: (data.djRolePerms ? data.djRolePerms.split(",") : base.dj).includes(permName),
@@ -36,23 +35,22 @@ class PermissionsHandler {
 			modRole: (data.modRolePerms ? data.modRolePerms.split(",") : base.mod).includes(permName),
 			extraRole2: (data.extraRole2Perms ? data.extraRole2Perms.split(",") : base.extraRole2).includes(permName),
 		};
-		
 		switch (hasRole) {
-		case "dj":
+			case "dj":
 
-			return inherits.dj || inherits.everyone;
-		case "extraRole":
-			return inherits.extraRole || inherits.dj || inherits.everyone;
+				return inherits.dj || inherits.everyone;
+			case "extraRole":
+				return inherits.extraRole || inherits.dj || inherits.everyone;
 
-		case "modRole":
-			
-			return inherits.modRole || inherits.extraRole || inherits.dj || inherits.everyone;
+			case "modRole":
 
-		case "extraRole2":
-			return inherits.extraRole2 || inherits.modRole || inherits.extraRole || inherits.dj || inherits.everyone;
+				return inherits.modRole || inherits.extraRole || inherits.dj || inherits.everyone;
 
-		default:
-			return inherits.everyone;
+			case "extraRole2":
+				return inherits.extraRole2 || inherits.modRole || inherits.extraRole || inherits.dj || inherits.everyone;
+
+			default:
+				return inherits.everyone;
 
 		}
 
@@ -69,7 +67,7 @@ class PermissionsHandler {
 		};
 		return item[roleName];
 	}
-	getallPerms(){
+	getallPerms() {
 		return allPerms;
 	}
 }
