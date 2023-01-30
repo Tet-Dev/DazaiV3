06c91e6f7b582db90b02559efc1ddb2eb15a00b4
John Li (Tet) <tet@tet.moe>
2023-01-30 01:07:07 -0800

    add build + push rFSync

--- src/Server/server.ts
+++ src/Server/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { RESTHandler } from '../types/misc';
 import { getDiscordUser } from './Utils/getDiscordUser';
 import { join } from 'path';
+import { readFileSync } from "fs";
 const server = express();
 server.use(cors());
 server.use(express.json());
