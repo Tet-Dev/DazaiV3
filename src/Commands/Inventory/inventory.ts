c711700db215c00e8b39c363aef7389df863fb04
John Li (Tet) <tet@tet.moe>
2023-02-15 14:21:33 -0800

    added free crate

--- src/Commands/Inventory/inventory.ts
+++ src/Commands/Inventory/inventory.ts
@@ -8,14 +8,18 @@ import {
   InteractionDataOptionsUser,
   Member,
 } from 'eris';
-import { CardRarity, CardType, Crate, rarityEmojiMap } from '../../constants/cardNames';
+import {
+  CardRarity,
+  CardType,
+  Crate,
+  rarityEmojiMap,
+} from '../../constants/cardNames';
 import { getCard } from '../../Handlers/Crates/CardManager';
 import { CrateManager } from '../../Handlers/Crates/CrateManager';
 import { InventoryManager } from '../../Handlers/Crates/InventoryManager';
 import { InteractionCollector } from '../../Handlers/InteractionCollector';
 import { MusicManager } from '../../Handlers/Music/MusicPlayer';
 import { Command } from '../../types/misc';
-
 
 const parseInventoryItem = (
   item: {
@@ -271,6 +275,69 @@ export const inventory = {
       msg,
       1000 * 120
     );
+    if (interaction.guildID === '739559911033405592') {
+      // check crate count
+      const userCrates = await CrateManager.getInstance().getUserCrates(
+        interaction.member ? interaction.member.user.id : interaction.user?.id!,
+        interaction.guildID,
+        true
+      );
+      if (userCrates.length < 2) {
+        const crateTemplate = await CrateManager.getInstance().getCrateTemplate(
+          `63eb39f288bdaa3a2df23e35`
+        );
+        if (!crateTemplate) return;
+        // random between 2-4 crates
+        const crateCount = Math.floor(Math.random() * 4) + 3;
+        for (let i = 0; i < crateCount; i++)
+          await CrateManager.getInstance().generateCrate(
+            crateTemplate,
+            interaction.guildID,
+            interaction.member
+              ? interaction.member.user.id
+              : interaction.user?.id!
+          );
+
+        interaction.createFollowup({
+          embeds: [
+            {
+              title: `Free Tet Dev Crates!`,
+              description: `As a new user, you have been given \`${crateCount}\` crates! You can open them by using going to the inventory and clicking on the crates!`,
+            },
+          ],
+        });
+      }
+    }
+    const userCrates = (await CrateManager.getInstance().getUserCrates(
+      interaction.member ? interaction.member.user.id : interaction.user?.id!,
+      `@global`,
+      true
+    )) as Crate[];
+    if (userCrates.filter((x) => x.guildID === '@global').length < 2) {
+      const crateTemplate = await CrateManager.getInstance().getCrateTemplate(
+        `63eb4ebb0296c1c2c951ba82`
+      );
+      if (!crateTemplate) return console.log(`Crate template not found!`);
+      // random between 2-4 crates
+      const crateCount = Math.floor(Math.random() * 3) + 2;
+      for (let i = 0; i < crateCount; i++)
+        await CrateManager.getInstance().generateCrate(
+          crateTemplate,
+          `@global`,
+          interaction.member
+            ? interaction.member.user.id
+            : interaction.user?.id!
+        );
+
+      interaction.createFollowup({
+        embeds: [
+          {
+            title: `Free Crates!`,
+            description: `As a new user, you have been given \`${crateCount}\` crates! You can open them by using going to the inventory and clicking on the crates!`,
+          },
+        ],
+      });
+    }
   },
 } as Command;
 
