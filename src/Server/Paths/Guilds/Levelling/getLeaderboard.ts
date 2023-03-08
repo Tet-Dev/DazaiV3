277b8aff16f36d3d799a028dd57b14641f3ea955
John Li (Tet) <tet@tet.moe>
2023-03-08 01:42:03 -0800

    debug timers

--- src/Server/Paths/Guilds/Levelling/getLeaderboard.ts
+++ src/Server/Paths/Guilds/Levelling/getLeaderboard.ts
@@ -15,20 +15,34 @@ export const getLeaderboard = {
     if (Number.isNaN(offset) || Number.isNaN(limit)) {
       return res.status(400).json({ error: 'Invalid offset or limit' });
     }
+    let timer = Date.now();
+    console.log('leaderboard timing start');
     const leaderboard = await XPManager.getInstance().getLeaderboard(
       guildID,
 
       Math.min(limit, 100),
       offset
     );
+    console.log('leaderboard timing end', Date.now() - timer);
+    let getUserTime = 0;
+    let getCardTime = 0;
     let resultMap = await Promise.all(
       leaderboard.map(async (xp) => {
+        let start = Date.now();
         let user =
           bot.users.get(xp.userID) ||
           (await bot
             .getRESTGuildMember(guildID, xp.userID)
             .catch((e) => null)) ||
           (await bot.getRESTUser(xp.userID).catch((e) => null));
+        getUserTime += Date.now() - start;
+        start = Date.now();
+        let card = await InventoryManager.getInstance()
+          .getSelectedCard(xp.userID, guildID)
+          .then((x) => (x ? x.url : null))
+          .catch((e) => null);
+        console.log('leaderboard timing card', Date.now() - start);
+        getCardTime += Date.now() - start;
         return {
           ...xp,
           user: {
@@ -39,12 +53,13 @@ export const getLeaderboard = {
             banner: user?.banner,
             color: user?.accentColor,
           },
-          card: await InventoryManager.getInstance()
-            .getSelectedCard(xp.userID, guildID)
-            .then((x) => (x ? x.url : null)),
+          card: card,
         };
       })
     );
+    console.log('leaderboard timing end2', Date.now() - timer);
+    console.log('leaderboard timing getUserTime', getUserTime);
+    console.log('leaderboard timing getCardTime', getCardTime);
     return res.json(resultMap);
   },
 } as RESTHandler;
