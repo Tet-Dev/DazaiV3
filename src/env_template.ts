dbc27a5a0f3c2bab04ca6cbc829c4197c1897029
John Li (Tet) <tet@tet.moe>
2023-05-02 20:20:11 -0700

    added env template

---
+++ src/env_template.ts
@@ -0,0 +1,37 @@
+import { NodeOptions } from 'erela.js';
+
+export const env = {
+  token: '',
+  LavalinkNodes: [
+    {
+      host: '',
+      port: 0,
+      password: '',
+    },
+  ] as NodeOptions[],
+  google: {
+    type: 'service_account',
+    project_id: '',
+    private_key_id: '',
+    private_key: '',
+    client_email: '',
+    client_id: '',
+    auth_uri: '',
+    token_uri: '',
+    auth_provider_x509_cert_url: '',
+    client_x509_cert_url: '',
+  },
+  MongoURL: 'mongodb+srv://YOUR MONGO URL',
+  MusicDrawers: 1, //How many threads should be used to generate music cards
+  SlanderDrawers: 1, //How many threads should be used to generate slander cards
+  RankCardDrawers: 2, //How many threads should be used to generate rank cards
+  webserver: null,
+  port: 888,
+  website: 'http://localhost:3000',
+  devmode: true,
+  sql: null,
+  voteKey: '',
+  adminID: ``,
+};
+export default env;
+export type EnvData = typeof env;
