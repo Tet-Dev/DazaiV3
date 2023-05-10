40a55220f6ec217ffb322e781c464d6240accd6c
9t8 <63334933+9t8@users.noreply.github.com>
2023-05-09 22:59:23 -0700

    delete OldStuff and OldSRC

--- OldSRC/Handlers/PermissionsHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Member } from 'eris';
-export const PermissionNodes = {
-  migrateData: 'migrate',
-}
-export const checkForPermission = (member : Member, permission : string) => {
-  member;
-  permission;
-  return false;
-}
--- OldSRC/types/global.d.ts
+++
@@ -1,6 +0,0 @@
-import { EnvData } from '../env';
-
-namespace globalThis {
-    export const env: EnvData;
-}
-export {};
